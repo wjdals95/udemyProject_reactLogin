19181ec68541e38a15c7401d7f1f93bd067e0fab
wjdals95 <wjdals992@naver.com>
2023-02-17 11:54:27 +0900

    Context로 인증 State 관리하기

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,13 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
 
+import AuthContext from "../../store/auth-context";
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+  
+  const authCtx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -49,7 +52,9 @@ const AuthForm = () => {
           throw new Error(errorMessage);
         });
       }
-    }).then((data)=>{}).catch((err) => {
+    }).then((data)=>{
+      authCtx.login(data.idToken);
+    }).catch((err) => {
       alert(err.message);
     });
   };
--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,24 +1,35 @@
-import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
+import AuthContext from "../../store/auth-context";
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggenIn = authCtx.isLoggedIn;
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!isLoggenIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoggenIn && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
+          {isLoggenIn && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
