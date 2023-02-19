7d2e8cbfabad377aaef7a19d04bbafaf63980a30
wjdals95 <wjdals992@naver.com>
2023-02-19 17:18:45 +0900

    사용자 인증 state(localstorage사용하여) 유지하기

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,25 +8,32 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+    const initialToken = localStorage.getItem('token')
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
 
   const contextValue = {
     token: token,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
-    logout: logoutHandler
-  }
-  return <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>;
+    logout: logoutHandler,
+  };
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
